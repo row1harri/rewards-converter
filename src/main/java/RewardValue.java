3a57706ee06663abc5a5031cf1fee87afda10064
Rowan Harris <rowanharris0702@gmail.com>
2024-08-17 20:51:08 +0100

    created RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+
+public class RewardValue{
+    public static BigDecimal RewardValue(){
+
+    }
+
+    public static double RewardValue(){
+
+    }
+
+    public static  get(){
+
+    }
+}
