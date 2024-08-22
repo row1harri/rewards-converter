be734cdc0acb050796a1cababc60630cc877f40c
Rowan Harris <rowanharris0702@gmail.com>
2024-08-22 12:26:24 +0100

    create the RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,16 +1,26 @@
-import java.util.Scanner;
-import java.math.BigDecimal;
+
 
 public class RewardValue{
-    public static BigDecimal RewardValue(){
+    static final double mToc = 0.0035; //miles to cash conversion rate
+    static final double cTom = 1/mToc; //cash to miles conversion rate
+    static int miles;
+    static double cash;
 
+    public RewardValue(double cashVal){
+        cash = cashVal;
+        miles = (int)(cashVal * cTom);
     }
 
-    public static double RewardValue(){
-
+    public RewardValue(int mileage){
+        miles = mileage;
+        cash = mileage * mToc;
     }
 
-    public static  get(){
+    public static double getCashValue(){
+        return cash;
+    }
 
+    public static int getMilesValue(){
+        return miles;
     }
 }
